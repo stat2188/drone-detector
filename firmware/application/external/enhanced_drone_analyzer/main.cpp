60e7371bbd4df879831f39b2526747462cd4180b
Max2188 <maxstatic2188@gmail.com>
2025-11-28 11:30:57 +0500

    fx

--- firmware/application/external/enhanced_drone_analyzer/main.cpp
+++ firmware/application/external/enhanced_drone_analyzer/main.cpp
@@ -1,16 +1,14 @@
 /*
  * Enhanced Drone Analyzer Scanner App
- * Fixed main.cpp for Mayhem Firmware
+ * Fixed Section Name for Linker
  */
 
 #include "ui_navigation.hpp"
 #include "external_app.hpp"
 #include "portapack.hpp"
 
-// Подключаем ваш объединенный заголовочный файл, где объявлен EnhancedDroneSpectrumAnalyzerView
 #include "ui_scanner_combined.hpp"
 
-// Если VERSION_MD5 не определен системой сборки, задаем заглушку
 #ifndef VERSION_MD5
 #define VERSION_MD5 1
 #endif
@@ -19,24 +17,22 @@ using namespace portapack;
 
 namespace ui::external_app::enhanced_drone_analyzer {
 
-// Функция инициализации, которая будет вызвана при запуске приложения
 void initialize_app(ui::NavigationView& nav) {
-    // Запускаем основное окно (View), определенное в ui_scanner_combined.hpp
     nav.push<EnhancedDroneSpectrumAnalyzerView>();
 }
 
 } // namespace ui::external_app::enhanced_drone_analyzer
 
-// Структура регистрации внешнего приложения
 extern "C" {
 
-// Атрибут section важен для того, чтобы загрузчик нашел эту структуру в начале бинарного файла
-__attribute__((section(".external_app.app_enhanced_drone_analyzer_scanner.application_information"), used)) 
-application_information_t _application_information_enhanced_drone_analyzer_scanner = {
+// !!! КРИТИЧЕСКОЕ ИСПРАВЛЕНИЕ !!!
+// Имя секции должно быть: .external_app.<ИМЯ_ПАПКИ>.application_information
+// Ваша папка называется: enhanced_drone_analyzer
+__attribute__((section(".external_app.enhanced_drone_analyzer.application_information"), used)) 
+application_information_t enhanced_drone_analyzer_application_information = {
     
-    /*.memory_location = */ (uint8_t*)0x00000000, // Заполняется загрузчиком
+    /*.memory_location = */ (uint8_t*)0x00000000,
     
-    // Указатель на функцию запуска
     /*.externalAppEntry = */ ui::external_app::enhanced_drone_analyzer::initialize_app, 
     
     /*.header_version = */ CURRENT_HEADER_VERSION,
@@ -44,7 +40,6 @@ application_information_t _application_information_enhanced_drone_analyzer_scann
 
     /*.app_name = */ "EDA Scanner",
 
-    // Иконка 32x32
     /*.bitmap_data = */ {
         0x00, 0x00, 0x80, 0x01, 0xC0, 0x03, 0xE0, 0x07, 0xF0, 0x0F, 0xF8, 0x1F,
         0xFC, 0x3F, 0xFE, 0x7F, 0xFE, 0x7F, 0xFC, 0x3F, 0xF8, 0x1F, 0xF0, 0x0F,
@@ -52,15 +47,13 @@ application_information_t _application_information_enhanced_drone_analyzer_scann
     },
 
     /*.icon_color = */ ui::Color::red().v,
-    /*.menu_location = */ app_location_t::RX, // Раздел Receivers
-    /*.desired_menu_position = */ 1,          // Позиция в меню (по возможности)
+    /*.menu_location = */ app_location_t::RX,
+    /*.desired_menu_position = */ 1,
 
-    // !!! ИСПРАВЛЕНИЕ ОШИБКИ !!!
-    // Число 0x4D465750 заменено на массив символов.
-    // PWFM - это тег приложения Wideband FM, который нужен для работы спектра
+    // Тег Baseband (PWFM) как массив символов
     /*.m4_app_tag = */ { 'P', 'W', 'F', 'M' },
 
-    /*.m4_app_offset = */ 0x00000000 // Заполняется при компиляции
+    /*.m4_app_offset = */ 0x00000000 
 };
 
 }
