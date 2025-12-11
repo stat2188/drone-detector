8a4eedea8103138242b31b6a912d60fa7c663e13
Max2188 <maxstatic2188@gmail.com>
2025-12-11 14:20:10 +0500

    fx

--- firmware/tools/external_app_info.py
+++ firmware/tools/external_app_info.py
@@ -25,9 +25,8 @@
 maximum_application_size = 32*1024
 # Special sizes for specific apps
 special_app_sizes = {
-    'enhanced_drone_analyzer': 56*1024,
+    'enhanced_drone_analyzer': 50*1024,
     'enhanced_drone_analyzer_settings': 10*1024,
-    'battleship': 2*1024,
 }
 external_apps_address_start = 0xADB00000
 external_apps_address_end = 0xADF00000
