35d75a1a2985a92eebd93052b3f8a7845289e22a
Max2188 <maxstatic2188@gmail.com>
2026-04-18 15:17:22 +0500

    11

--- firmware/application/apps/enhanced_drone_analyzer/pattern_types.cpp
+++ firmware/application/apps/enhanced_drone_analyzer/pattern_types.cpp
@@ -3,13 +3,13 @@
 namespace drone_analyzer {
 
 SignalPattern::SignalPattern() noexcept
-    : waveform{}
-    , features{}
+    : name{}                     // Zero-initialize char array (C++11 aggregate init)
+    , waveform{}                  // Zero-initialize uint8_t array (C++11 aggregate init)
+    , features{}                   // PatternFeatures has member initializer list in struct
     , match_threshold(DEFAULT_PATTERN_CORRELATION_THRESHOLD)
     , flags(SignalPattern::Flags::ENABLED)
     , created_time(0)
     , match_count(0) {
-    memset(name, 0, PATTERN_NAME_MAX_LEN);
 }
 
 } // namespace drone_analyzer
