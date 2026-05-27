321a4c5371c15083e2aac0f47683df527edb0b66
Max2188 <maxstatic2188@gmail.com>
2026-05-27 12:09:53 +0500

    SpectrumPreviewWidget: pixel-resolution rendering with 240 bins for detailed peak shape

--- firmware/application/apps/enhanced_drone_analyzer/spectrum_preview_widget.hpp
+++ firmware/application/apps/enhanced_drone_analyzer/spectrum_preview_widget.hpp
@@ -2,11 +2,8 @@
 #define SPECTRUM_PREVIEW_WIDGET_HPP
 
 #include <cstdint>
-#include <array>
 #include "ui_widget.hpp"
 #include "ui_painter.hpp"
-#include "spectrum_shape.hpp"
-#include "constants.hpp"
 
 namespace drone_analyzer {
 
@@ -30,26 +27,16 @@ public:
         uint8_t symmetry) noexcept;
 
 private:
-    struct Params {
-        uint8_t margin{DEFAULT_SPECTRUM_MARGIN};
-        uint8_t min_width{DEFAULT_SPECTRUM_MIN_WIDTH};
-        uint8_t max_width{DEFAULT_SPECTRUM_MAX_WIDTH};
-        uint8_t sharpness{DEFAULT_SPECTRUM_PEAK_SHARPNESS};
-        uint8_t peak_ratio{DEFAULT_SPECTRUM_PEAK_RATIO};
-        uint8_t valley_depth{DEFAULT_SPECTRUM_VALLEY_DEPTH};
-        uint8_t flatness{DEFAULT_SPECTRUM_FLATNESS};
-        uint8_t symmetry{DEFAULT_SPECTRUM_SYMMETRY};
-    };
+    uint8_t margin_{20};
+    uint8_t min_width_{5};
+    uint8_t max_width_{200};
+    uint8_t sharpness_{130};
+    uint8_t peak_ratio_{0};
+    uint8_t valley_depth_{80};
+    uint8_t flatness_{30};
+    uint8_t symmetry_{50};
 
-    Params params_;
-    std::array<uint8_t, FFT_BIN_COUNT> synthetic_spectrum_{};
-    std::array<uint8_t, FFT_BIN_COUNT> sort_buf_{};
-    SpectrumShape::Config shape_config_{};
-    SpectrumShape::AnalysisResult result_{};
-    bool dirty_{true};
-
-    void recompute() noexcept;
-    static ui::Color amplitude_color(uint8_t value, uint8_t min_val, uint8_t max_val) noexcept;
+    static ui::Color amplitude_color(int32_t h, int32_t max_h) noexcept;
 };
 
 } // namespace drone_analyzer
